357f5bd245c816a2be66d923916e1d1da597338d
Luke Morgan <morgan.luke94@gmail.com>
2019-12-28 16:20:27 +0000

    adds inital lead js logic

--- app/views/update-view.php
+++ app/views/update-view.php
@@ -8,17 +8,19 @@ require_once __DIR__.'/../../public/template-parts/header.php'; ?>
     <h3 class="mb-4">Update your lead details by selecting the fields you wish to update</h3>
     <div class="row">
         <div class="col-md-8 col-sm-12">
-            <form method="post" class="form p-4 card">
+            <form method="post" class="form p-4 card" id="update-lead-form">
+                <input type="hidden" name="id" value="<?= $lead->id?>">
+                <input type="hidden" name="_method" value="update">
                 <h4>Update <?= $lead->company_name; ?></h4>
 
             </form>
         </div>
         <div class="col-md-4 col-sm-12">
             <div class="card p-4">
-                <button id="#company-name" class="btn btn-secondary form-control mb-3">Company name</button>
-                <button id="#company-contact" class="btn btn-secondary form-control mb-3">Company contact</button>
-                <button id="#company-role" class="btn btn-secondary form-control mb-3">Contact role</button>
-                <button id="#company-contact-email" class="btn btn-secondary form-control">Company contact
+                <button id="company-name" class="btn btn-secondary form-control mb-3">Company name</button>
+                <button id="company-contact" class="btn btn-secondary form-control mb-3">Company contact</button>
+                <button id="contact-role" class="btn btn-secondary form-control mb-3">Contact role</button>
+                <button id="contact-email" class="btn btn-secondary form-control">Company contact
                     email</button>
             </div>
         </div>
--- public/js/updateView.js
+++ public/js/updateView.js
@@ -1,3 +1,29 @@
-const updateController = (function() {
-  alert("success");
+const updateLeadController = (function() {
+  const domElements = {
+    updateForm: document.querySelector("#update-lead-form"),
+    updateCompanyName: document.querySelector("#company-name"),
+    updateContactName: document.querySelector("#company-contact"),
+    updateContactRole: document.querySelector("#contact-role"),
+    updateContactEmail: document.querySelector("#contact-email")
+  };
+  const appendFormHtml = {
+    updateCompany: function() {
+      let formGroup = document.createElement("div");
+      formGroup.classList.add("form-group");
+      let label = document.createElement("label");
+      label.innerText = "Update company name:";
+      formGroup.appendChild(label);
+      domElements.updateForm.appendChild(formGroup);
+    },
+    test: function() {
+      console.log("clicked");
+    }
+  };
+  const eventListeners = [
+    domElements.updateCompanyName.addEventListener(
+      "click",
+      appendFormHtml.updateCompany
+    )
+  ];
+  return eventListeners;
 })();
