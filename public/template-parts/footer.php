46f61fbc86a09d077a4df0de1394cc42300aa6eb
Luke Morgan <morgan.luke94@gmail.com>
2020-01-01 04:37:35 +0000

    adds functions for removeing form fields in update view

--- public/template-parts/footer.php
+++ public/template-parts/footer.php
@@ -17,7 +17,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
     integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
 </script>
-
+<script src="https://kit.fontawesome.com/f2ad0cb5b1.js" crossorigin="anonymous"></script>
 <script src="public/js/updateView.js"></script>
 <script src="public/js/manageView.js"></script>
 
